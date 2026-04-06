1d130668d6fb64eafa438a54f971728148b3e448
createdreallol <116285995+createdreallol@users.noreply.github.com>
2026-04-05 22:02:49 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,13 @@
-	print("It worked...")
-    wait(20)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/createdreallol/yessuh/refs/heads/main/mm2.lua"))()
+print("It worked...")
+
+task.wait(20)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/createdreallol/yessuh/refs/heads/main/mm2.lua"))()
 
 pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/createdreallol/yessuh/refs/heads/main/gui.lua"))()
 end)
 
 pcall(function()
-    loadstring(game:HttpGet('https://raw.smokingscripts.org/vertex.lua'))()
+    loadstring(game:HttpGet("https://raw.smokingscripts.org/vertex.lua"))()
 end)
