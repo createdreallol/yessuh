f7b40b064c6f0cc71742ccbb1497d82bb3ded624
createdreallol <116285995+createdreallol@users.noreply.github.com>
2026-07-26 03:14:52 -0400

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/6cda7723ea3003c47f81f6980086e7d45a542a883272afb2c6aff811733e855e/download"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjM4MTM3ODUzLTFhZmEtNDFjZC1hNjQ0LTVmNWM1YjcwOWI2MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJzdW1tZXJldmVudCJ9fQ"))()
